9cc9d792d1a3e90df3c737c562bd224f1fce38c0
Jason Wesson <jwesson@codeforamerica.org>
2022-09-04 15:35:42 -0700

    Add edit and update capabilities to portfolio item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -18,6 +18,23 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
 end
 
 private
